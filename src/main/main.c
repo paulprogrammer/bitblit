374f05b4f1076f957fa8d3819f738c3e5c002d45
Paul Williams <williamspaul@google.com>
2023-12-27 17:45:39 -0700

    now showing a window

--- src/main/main.c
+++ src/main/main.c
@@ -10,17 +10,35 @@
 int main(int argc, char ** argv) {
     logger(DEBUG, "Starting");
 
-    SDL_Window * win = SDL_CreateWindow("foo", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 320,200, SDL_WINDOW_SHOWN);
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_Window * win = SDL_CreateWindow("foo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 320,200, SDL_WINDOW_SHOWN);
     if( win == NULL) {
         logger(ERROR, SDL_GetError());
     }
 
+    SDL_UpdateWindowSurface(win);
+
     SDL_Surface * windowSurface = SDL_GetWindowSurface(win);
     SDL_FillRect(windowSurface, NULL, SDL_MapRGB(windowSurface->format,
                                                  0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(win);
 
-    SDL_Delay(5000);
+    SDL_Event e;
+    int keep_open=1;
+    while( keep_open) {
+        while (SDL_PollEvent(&e) > 0) {
+            switch(e.type)
+            {
+                case SDL_QUIT:
+                    keep_open = 0;
+                    break;
+            }
+            SDL_UpdateWindowSurface(win);
+        }
+    }
 
+    SDL_DestroyWindowSurface( win);
     SDL_DestroyWindow(win);
     SDL_Quit();
 
